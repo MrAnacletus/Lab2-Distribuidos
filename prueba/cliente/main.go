8d1ba2016b7d0199192ae9588d02d991b7118914
Sebastián Ignacio Sepúlveda Barría <sebastian.sepulvedab@sansano.usm.cl>
2021-11-05 12:34:40 -0300

    modified:   cliente/main.go 	renamed:    proto/comunicacion.pb.go -> proto/github.com/MrAnacletus/Lab2-Distribuidos/prueba/proto/comunicacion.pb.go 	renamed:    proto/comunicacion_grpc.pb.go -> proto/github.com/MrAnacletus/Lab2-Distribuidos/prueba/proto/comunicacion_grpc.pb.go

--- prueba/cliente/main.go
+++ prueba/cliente/main.go
@@ -19,9 +19,4 @@ func main(){
 		panic(err)
 	}
 	fmt.Println(res.Message)
-	res, err := serviceCLient.SayHelloAgain(context.Background(), &pb.HelloRequest{Name: "Anacletus"})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(res.Message)
 }
