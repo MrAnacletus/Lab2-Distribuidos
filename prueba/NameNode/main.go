1656747ce06a51407c63f6f72afa3ba45e64e02a
Sebastián Ignacio Sepúlveda Barría <sebastian.sepulvedab@sansano.usm.cl>
2021-11-06 17:51:32 -0300

    modified:   prueba/NameNode/main.go

--- prueba/NameNode/main.go
+++ prueba/NameNode/main.go
@@ -1,11 +1,14 @@
 package main
 
-import(
-	pb "github.com/MrAnacletus/Lab2-Distribuidos/prueba/proto"
-	"google.golang.org/grpc"
+import (
 	"context"
 	"fmt"
+	"log"
+	"math/rand"
 	"net"
+
+	pb "github.com/MrAnacletus/Lab2-Distribuidos/prueba/proto"
+	"google.golang.org/grpc"
 )
 
 type server struct{
@@ -21,11 +24,30 @@ func (s *server) SayHelloAgain(ctx context.Context, in *pb.HelloRequest) (*pb.He
 	return &pb.HelloReply{Message: "Hello "}, nil
 }
 
-func (s *server) GetJugada(ctx context.Context, in *pb.Jugada) (*pb.HelloReply, error) {
-	fmt.Println("Jugada recibida, Jugada: " + fmt.Sprint(in.Jugada) + " Jugador: " + fmt.Sprint(in.ID))
-	return &pb.HelloReply{Message: "Jugadas recibidas, gracias"}, nil
-}
+func (s *server) GetJugada(ctx context.Context, in *pb.Jugada) (*pb.Resultado, error) {
+	// Enviarla a NameNode
+	fmt.Println("Jugadas recibidas, jugada:" + fmt.Sprint(in.GetJugada()) + " del jugador: " + fmt.Sprint(in.GetID()))
+	conn, err := grpc.Dial("10.6.40.219:8080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("No se pudo conectar con el server NameNode: %v",err)
+	}
+	defer conn.Close()
 
+	//Se crea un cliente para la conexion
+	serviceClient := pb.NewHelloServiceClient(conn)
+	//Se envia la jugada apra que sea escrita en el archivo
+	_, err = serviceClient.GetJugada(context.Background(), &pb.Jugada{Jugada: in.GetJugada(), ID: in.GetID()})
+	if err != nil {
+		log.Fatalf("No se pudo enviar la jugada: %v",err)
+	}
+	//Lider debe elegir un numero entre 6 y 10
+	JugadaLider := rand.Intn(4) + 6
+	if JugadaLider <= int(in.GetJugada()){
+		//Notificar a pozo que alguien murio por rabbitmq
+		return &pb.Resultado{ID: in.GetID(),Estado: 0}, nil
+	}
+	return &pb.Resultado{ID: in.GetID(),Estado: 1}, nil
+}
 func ServidorNameNode(){
 	listener , err := net.Listen("tcp", ":8080")
 	if err != nil{
